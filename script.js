d6eaa0eb3e27a0e8ebe137bf45008b53d1fd4317
Aurore <hocquela@gmail.com>
2025-11-16 21:04:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Bienvenue dans les coulisses de mon site ✨");
+
+  // Petit message d'accueil
+  alert("Mon premier site web est en marche !");
+});
