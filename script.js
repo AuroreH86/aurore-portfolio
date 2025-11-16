00b029b2cb1ca9c595fbf9e812e87d50d3e58a6a
Aurore <hocquela@gmail.com>
2025-11-16 21:31:28 +0100

    css et js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Bienvenue dans les coulisses de mon site ✨");
-
-  // Petit message d'accueil
-  alert("Mon premier site web est en marche !");
-});
---
+++ scripts/ui.js
@@ -0,0 +1,1 @@
+/**composants réutilisables*/
